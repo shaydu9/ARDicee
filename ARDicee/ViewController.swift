57b5f156097dff5bec6dbbb08e452dda7db0617f
Shay Dubrovsky <shaydu9@gmail.com>
2023-12-30 12:04:38 +0200

    Moon sphere

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -22,23 +22,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        let sphere = SCNSphere(radius: 0.2)
         
         let material = SCNMaterial()
         
-        material.diffuse.contents = UIColor.orange
+//        material.diffuse.contents = UIColor.orange
+        material.diffuse.contents = UIImage(named: "art.snassets/8k_moon.jpg")
         
-        cube.materials = [material]
+        sphere.materials = [material]
         
         let node = SCNNode()
         
         node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
         
-        node.geometry = cube
+        node.geometry = sphere
         
         sceneView.scene.rootNode.addChildNode(node)
         
-        
+        sceneView.autoenablesDefaultLighting = true
         
         
 //        // Create a new scene
